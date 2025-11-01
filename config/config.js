d55540c8d69cc0b88563795652212a39bedb9d68
dla-tech <darroyolebarro@hotmail.com>
2025-11-01 10:29:15 -0400

    Actualizar config.js

--- config/config.js
+++ config/config.js
@@ -171,6 +171,6 @@ window.APP_CONFIG = {
     notifDefaults: { image: "https://example.com/fallback.jpg" },
 
     // 🟡 Texto del banner flotante de anuncio (se muestra al entrar)
-    promoText: "📣 Ayuno de Caballeros y Jovenes diciembre 5 al 7 separa la fecha🙌🏽"
+    promoText: "📣 ¡Ayuno de Caballeros y Jovenes! diciembre 5 al 7 del 2025,separa la fecha🙌🏽"
   }
 };
